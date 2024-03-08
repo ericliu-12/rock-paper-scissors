6a9a59efd60dccd3f520096da1d8b4b5b4a4c9c2
alaska <133845008+alaska-summer@users.noreply.github.com>
2024-03-07 18:34:06 -0600

    Add function that plays one round

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ function getComputerChoice() {
     else return "Scissors"
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "rock") return "You Tie! Rock ties Rock";
+    if (playerSelection === "rock" && computerSelection === "paper") return "You Lose! Paper beats Rock";
+    if (playerSelection === "rock" && computerSelection === "scissors") return "You Win! Rock beats Scissors"
+    if (playerSelection === "paper" && computerSelection === "rock") return "You Win! Paper beats Rock";
+    if (playerSelection === "paper" && computerSelection === "paper") return "You Tie! Paper ties Paper"
+    if (playerSelection === "paper" && computerSelection === "scissors") return "You Lose! Scissors beats Paper"
+    if (playerSelection === "scissors" && computerSelection === "rock") return "You Lose! Rock beats Scissors"
+    if (playerSelection === "scissors" && computerSelection === "paper") return "You Win! Scissors beats Paper"
+    if (playerSelection === "scissors" && computerSelection === "scissors") return "You Tie! Scissors ties Scissors"
+}
